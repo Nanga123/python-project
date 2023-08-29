15d972aa4a8bdc129ea708f4eb580dad996ba01e
Nanga123 <131233463+Nanga123@users.noreply.github.com>
2023-08-30 00:31:03 +0530

    Update views.py

--- views.py
+++ views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import signupform
 from qrcode import*
 data=None
 # Create your views here.
+def signupview(request):
+    if request.method=='POST':
+        form=signupform(request.POST)
+        if form.is_valid():
+            form.save
+            return redirect('login')
+    else:
+        form=signupform()
+    return render(request,'signup.html',{'form':form})
+    return render(request,'signup.html')
+
+def login1(request):
+    if request.method=="POST":
+        username=request.POST['username']
+        password=request.POST['passwd']
+
+        user=authenticate(username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return render('next')
+    else:
+        return render(request,'login.html')
+
 def homepage(request):
     global data
     if request.method=='POST':
@@ -11,3 +35,6 @@ def homepage(request):
     else:
         pass
     return render(request,'home.html')
+
+def Next(request):
+    return render(request,'next.html')
