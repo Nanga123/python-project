d90c70703a5371616b22dc4980955f0f2a08c7b3
Nanga123 <131233463+Nanga123@users.noreply.github.com>
2023-09-13 21:57:50 +0530

    Update views.py

--- views.py
+++ views.py
@@ -1,40 +1,81 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .forms import signupform
 from qrcode import*
+from django.contrib.auth import authenticate,login
+
+from django.template import RequestContext
+from django.shortcuts import HttpResponseRedirect
+
 data=None
+
 # Create your views here.
+
 def signupview(request):
+
     if request.method=='POST':
+
         form=signupform(request.POST)
+
         if form.is_valid():
-            form.save
-            return redirect('login')
+
+            form.save()
+            
+            return HttpResponseRedirect('login')
     else:
         form=signupform()
+
     return render(request,'signup.html',{'form':form})
+
     return render(request,'signup.html')
 
-def login1(request):
-    if request.method=="POST":
-        username=request.POST['username']
-        password=request.POST['passwd']
 
-        user=authenticate(username=username,password=password)
+def Login(request):
+
+    if request.method=="POST":
+
+        username=request.POST['username']
+
+        password=request.POST['password']
+
+
+        user= authenticate(username=username,password=password)
+
         if user is not None:
+
             login(request,user)
-            return render('next')
+            return HttpResponseRedirect('next')
+        else:
+            return HttpResponseRedirect('login')
     else:
+
         return render(request,'login.html')
 
 def homepage(request):
+
     global data
+
     if request.method=='POST':
+
         data=request.POST['data']
+
         img=make(data)
+
         img.save('demo/static/images/test.png')
     else:
         pass
+
     return render(request,'home.html')
 
+
 def Next(request):
+
     return render(request,'next.html')
+
+def Trending(request):
+
+    return render(request,'trending.html')
+
+
+
+
+
