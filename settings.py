46e9633d1446db5a9a606473e0880137c2cc2640
Nanga123 <131233463+Nanga123@users.noreply.github.com>
2023-09-15 01:37:43 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,3 +1,14 @@
 import os
 MEDIA_URL='/images/'
 STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'bookmyshow',
+        'HOST':'localhost',
+        'PORT':'3306', 
+        'USER':'root',
+        'PASSWORD':'sql123inlife'
+    }
+}
