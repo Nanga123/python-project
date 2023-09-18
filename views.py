2183f55725cee15d5888c3fb45d43f416edc26c0
Nanga123 <131233463+Nanga123@users.noreply.github.com>
2023-09-19 00:45:26 +0530

    Update views.py

--- views.py
+++ views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from .forms import signupform
 from qrcode import*
 from django.contrib.auth import authenticate,login
-
+import mysql.connector as mysc
+import tabulate as t
 from django.template import RequestContext
+from django.contrib import messages
 from django.shortcuts import HttpResponseRedirect
 
 data=None
@@ -45,6 +47,7 @@ def Login(request):
             login(request,user)
             return HttpResponseRedirect('next')
         else:
+            messages.success(request,("user id or password is incorrect"))
             return HttpResponseRedirect('login')
     else:
 
@@ -77,6 +80,54 @@ def Trending(request):
 
 def Home1(request):
     return render(request,'home1.html')
+
+def Oppenheimer(request):
+    conn=mysc.connect (user='root',host='localhost',passwd='sql123inlife',database='bookmyshow')
+    cur=conn.cursor()
+    qy='select moviename from movies where moviename="oppenheimer"'
+    cur.execute(qy)
+    data=cur.fetchall()
+    qy2='select cast from movies where moviename="oppenheimer"'
+    cur.execute(qy2)
+    data1=cur.fetchall()
+    qy3='select type from movies where moviename="oppenheimer"'
+    cur.execute(qy3)
+    data2=cur.fetchall()
+    qy4='select language from movies where moviename="oppenheimer"'
+    cur.execute(qy4)
+    data3=cur.fetchall()
+    qy5='select rating from movies where moviename="oppenheimer"'
+    cur.execute(qy5)
+    data4=cur.fetchall()
+    data5={
+        'movie_name':(data),
+        'movie_cast':(data1),
+        'movie_type':(data2),
+        'movie_lang':(data3),
+        'movie_rate':(data4),
+
+    }
+
+    
+    if request.method=="POST" :
+        name=request.POST['name']
+        phone_no=request.POST['PHONE_NO']
+        tickets=request.POST['tickets']
+        movie_details=" "
+        movie_details+="movie:Oppenheimer,type:thriller,rating:8.6,"
+        movie_details+=",username:" + str(name)
+        movie_details+=",phone_no:"+ str(phone_no)
+        movie_details+=",no_of_tickets:"+ str(tickets)
+        img1=make(movie_details)
+        img1.save('demo/static/images/test1.png')
+    else:
+
+        pass
+    return render(request,'oppenheimer.html',data5)
+
+
+
+
 
 
 
