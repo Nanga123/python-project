e456ff5a49461bba744f113c703e78cddbe9f6ea
Nanga123 <131233463+Nanga123@users.noreply.github.com>
2023-09-20 02:50:24 +0530

    Update views.py

--- views.py
+++ views.py
@@ -124,6 +124,254 @@ def Oppenheimer(request):
 
         pass
     return render(request,'oppenheimer.html',data5)
+def Jailer(request):
+    conn=mysc.connect (user='root',host='localhost',passwd='sql123inlife',database='bookmyshow')
+    cur=conn.cursor()
+    qy='select moviename from movies where moviename="jailer"'
+    cur.execute(qy)
+    data=cur.fetchall()
+    qy2='select cast from movies where moviename="jailer"'
+    cur.execute(qy2)
+    data1=cur.fetchall()
+    qy3='select type from movies where moviename="jailer"'
+    cur.execute(qy3)
+    data2=cur.fetchall()
+    qy4='select language from movies where moviename="jailer"'
+    cur.execute(qy4)
+    data3=cur.fetchall()
+    qy5='select rating from movies where moviename="jailer"'
+    cur.execute(qy5)
+    data4=cur.fetchall()
+    data5={
+        'movie_name':(data),
+        'movie_cast':(data1),
+        'movie_type':(data2),
+        'movie_lang':(data3),
+        'movie_rate':(data4),
+
+    }
+
+    
+    if request.method=="POST" :
+        name=request.POST['name']
+        phone_no=request.POST['PHONE_NO']
+        tickets=request.POST['tickets']
+        movie_details=" "
+        movie_details+="movie:jailer,type:action,rating:7.2,"
+        movie_details+=",username:" + str(name)
+        movie_details+=",phone_no:"+ str(phone_no)
+        movie_details+=",no_of_tickets:"+ str(tickets)
+        img1=make(movie_details)
+        img1.save('demo/static/images/test2.png')
+        return render(request,'jailer2.html')
+    else:
+
+        pass
+    return render(request,'jailer.html',data5)
+def Jawan(request):
+    conn=mysc.connect (user='root',host='localhost',passwd='sql123inlife',database='bookmyshow')
+    cur=conn.cursor()
+    qy='select moviename from movies where moviename="jawan"'
+    cur.execute(qy)
+    data=cur.fetchall()
+    qy2='select cast from movies where moviename="jawan"'
+    cur.execute(qy2)
+    data1=cur.fetchall()
+    qy3='select type from movies where moviename="jawan"'
+    cur.execute(qy3)
+    data2=cur.fetchall()
+    qy4='select language from movies where moviename="jawan"'
+    cur.execute(qy4)
+    data3=cur.fetchall()
+    qy5='select rating from movies where moviename="jawan"'
+    cur.execute(qy5)
+    data4=cur.fetchall()
+    data5={
+        'movie_name':(data),
+        'movie_cast':(data1),
+        'movie_type':(data2),
+        'movie_lang':(data3),
+        'movie_rate':(data4),
+
+    }
+
+    
+    if request.method=="POST" :
+        name=request.POST['name']
+        phone_no=request.POST['PHONE_NO']
+        tickets=request.POST['tickets']
+        movie_details=" "
+        movie_details+="movie:jawan,type:action,rating:7.7,"
+        movie_details+=",username:" + str(name)
+        movie_details+=",phone_no:"+ str(phone_no)
+        movie_details+=",no_of_tickets:"+ str(tickets)
+        img1=make(movie_details)
+        img1.save('demo/static/images/test3.png')
+        return render(request,'jawan2.html')
+    else:
+
+        pass
+    return render(request,'jawan.html',data5)
+
+def Demonslayer(request):
+    conn=mysc.connect (user='root',host='localhost',passwd='sql123inlife',database='bookmyshow')
+    cur=conn.cursor()
+    qy='select moviename from movies where moviename="Demon slayer:To the swordsmith village"'
+    cur.execute(qy)
+    data=cur.fetchall()
+    qy2='select cast from movies where moviename="Demon slayer:To the swordsmith village"'
+    cur.execute(qy2)
+    data1=cur.fetchall()
+    qy3='select type from movies where moviename="Demon slayer:To the swordsmith village"'
+    cur.execute(qy3)
+    data2=cur.fetchall()
+    qy4='select language from movies where moviename="Demon slayer:To the swordsmith village"'
+    cur.execute(qy4)
+    data3=cur.fetchall()
+    qy5='select rating from movies where moviename=" Demon slayer:To the swordsmith village"'
+    cur.execute(qy5)
+    data4=cur.fetchall()
+    data5={
+        'movie_name':(data),
+        'movie_cast':(data1),
+        'movie_type':(data2),
+        'movie_lang':(data3),
+        'movie_rate':(data4),
+
+    }
+
+    
+    if request.method=="POST" :
+        name=request.POST['name']
+        phone_no=request.POST['PHONE_NO']
+        tickets=request.POST['tickets']
+        movie_details=" "
+        movie_details+="movie:Demonslayer:Totheswordsmithvillage,type:anime,rating:7.4,"
+        movie_details+=",username:" + str(name)
+        movie_details+=",phone_no:"+ str(phone_no)
+        movie_details+=",no_of_tickets:"+ str(tickets)
+        img1=make(movie_details)
+        img1.save('demo/static/images/test4.png')
+        return render(request,'demonslayer2.html')
+    else:
+
+        pass
+    return render(request,'demonslayer.html',data5)
+def twenty(request):
+    conn=mysc.connect (user='root',host='localhost',passwd='sql123inlife',database='bookmyshow')
+    cur=conn.cursor()
+    qy='select moviename from movies where moviename="2018"'
+    cur.execute(qy)
+    data=cur.fetchall()
+    qy2='select cast from movies where moviename="2018"'
+    cur.execute(qy2)
+    data1=cur.fetchall()
+    qy3='select type from movies where moviename="2018"'
+    cur.execute(qy3)
+    data2=cur.fetchall()
+    qy4='select language from movies where moviename="2018"'
+    cur.execute(qy4)
+    data3=cur.fetchall()
+    qy5='select rating from movies where moviename="2018"'
+    cur.execute(qy5)
+    data4=cur.fetchall()
+    data5={
+        'movie_name':(data),
+        'movie_cast':(data1),
+        'movie_type':(data2),
+        'movie_lang':(data3),
+        'movie_rate':(data4),
+
+    }
+
+    
+    if request.method=="POST" :
+        name=request.POST['name']
+        phone_no=request.POST['PHONE_NO']
+        tickets=request.POST['tickets']
+        movie_details=" "
+        movie_details+="movie:2018,typesurvival,rating:8.4,"
+        movie_details+=",username:" + str(name)
+        movie_details+=",phone_no:"+ str(phone_no)
+        movie_details+=",no_of_tickets:"+ str(tickets)
+        img1=make(movie_details)
+        img1.save('demo/static/images/test5.png')
+        return render(request,'twenty2.html')
+    else:
+
+        pass
+    return render(request,'twenty.html',data5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
