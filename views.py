e4dc633945e57e48a80b9f3a865dbda6925f1e7b
Nanga123 <131233463+Nanga123@users.noreply.github.com>
2023-10-03 00:32:34 +0530

    Update views.py

--- views.py
+++ views.py
@@ -623,6 +623,62 @@ def Rope(request):
         pass
     return render(request,'Rope.html',data5)
 
+def Spiderman(request):
+    conn=mysc.connect (user='root',host='localhost',passwd='sql123inlife',database='bookmyshow')
+    cur=conn.cursor()
+    qy='select moviename from movies where moviename="Spider-Man:Across the Spider-Verse"'
+    cur.execute(qy)
+    data=cur.fetchall()
+    qy2='select cast from movies where moviename="Spider-Man:Across the Spider-Verse"'
+    cur.execute(qy2)
+    data1=cur.fetchall()
+    qy3='select type from movies where moviename="Spider-Man:Across the Spider-Verse"'
+    cur.execute(qy3)
+    data2=cur.fetchall()
+    qy4='select language from movies where moviename="Spider-Man:Across the Spider-Verse"'
+    cur.execute(qy4)
+    data3=cur.fetchall()
+    qy5='select rating from movies where moviename="Spider-Man:Across the Spider-Verse"'
+    cur.execute(qy5)
+    data4=cur.fetchall()
+    data5={
+        'movie_name':(data),
+        'movie_cast':(data1),
+        'movie_type':(data2),
+        'movie_lang':(data3),
+        'movie_rate':(data4),
+
+    }
+
+    
+    if request.method=="POST" :
+        name=request.POST['name']
+        phone_no=request.POST['PHONE_NO']
+        tickets=request.POST['tickets']
+        movie_details=" "
+        movie_details+="movie:SpiderMan:AcrosstheSpiderVerse,type:sciFi,rating:8.7,"
+        movie_details+=",username:" + str(name)
+        movie_details+=",phone_no:"+ str(phone_no)
+        movie_details+=",no_of_tickets:"+ str(tickets)
+        img1=make(movie_details)
+        img1.save('demo/static/images/test13.png')
+        return render(request,'spiderman2.html')
+    else:
+
+        pass
+    return render(request,'spiderman.html',data5)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
