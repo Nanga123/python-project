e3cfb1b8bcfdf1fbd5c3a69ff497ad885ea6d2f6
Nanga123 <131233463+Nanga123@users.noreply.github.com>
2023-10-02 21:44:50 +0530

    Update views.py

--- views.py
+++ views.py
@@ -348,6 +348,52 @@ def CatchMeIfYouCan(request):
     return render(request,'CatchMeIfYouCan.html',data5)
 
 
+def Life(request):
+    conn=mysc.connect (user='root',host='localhost',passwd='sql123inlife',database='bookmyshow')
+    cur=conn.cursor()
+    qy='select moviename from movies where moviename="Life"'
+    cur.execute(qy)
+    data=cur.fetchall()
+    qy2='select cast from movies where moviename="Life"'
+    cur.execute(qy2)
+    data1=cur.fetchall()
+    qy3='select type from movies where moviename="Life"'
+    cur.execute(qy3)
+    data2=cur.fetchall()
+    qy4='select language from movies where moviename="Life"'
+    cur.execute(qy4)
+    data3=cur.fetchall()
+    qy5='select rating from movies where moviename="Life"'
+    cur.execute(qy5)
+    data4=cur.fetchall()
+    data5={
+        'movie_name':(data),
+        'movie_cast':(data1),
+        'movie_type':(data2),
+        'movie_lang':(data3),
+        'movie_rate':(data4),
+
+    }
+
+    
+    if request.method=="POST" :
+        name=request.POST['name']
+        phone_no=request.POST['PHONE_NO']
+        tickets=request.POST['tickets']
+        movie_details=" "
+        movie_details+="movie:Life,type:Thriller,rating:6.6,"
+        movie_details+=",username:" + str(name)
+        movie_details+=",phone_no:"+ str(phone_no)
+        movie_details+=",no_of_tickets:"+ str(tickets)
+        img1=make(movie_details)
+        img1.save('demo/static/images/test7.png')
+        return render(request,'Life2.html')
+    else:
+
+        pass
+    return render(request,'Life.html',data5)
+
+
 
 
 
